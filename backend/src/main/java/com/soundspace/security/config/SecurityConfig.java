8bf960258eeb20e3e4c6478c7dc58000b66414fa
JulianDemucha <demuchajulian.it@gmail.com>
2025-11-11 20:27:18 +0100

    feat(security): configure apiSecurityFilterChain for stateless jwt cookie and csrf cookie

--- backend/src/main/java/com/soundspace/security/config/SecurityConfig.java
+++ backend/src/main/java/com/soundspace/security/config/SecurityConfig.java
@@ -1,25 +1,48 @@
 package com.soundspace.security.config;
 
+import com.soundspace.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers("/api/auth/register", "/api/auth/authenticate")
+                )
+                .securityMatcher("/api/**")
+                .sessionManagement(sess ->
+                        sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/api/users/me", "/api/auth/register"
+                                , "/api/auth/authenticate").permitAll()
+                        .anyRequest().authenticated()
+                ).formLogin(AbstractHttpConfigurer::disable)
+                .authenticationProvider(authenticationProvider)
+                // w przyszlosci po wprowadzeniu oauth2, bedzie do niego oddzielny filterchain
+//                .oauth2Login(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+
 }
