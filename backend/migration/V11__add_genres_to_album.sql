3e8657c00863e0d1ad06a8594b75fb46d9ba341a
JulianDemucha <demuchajulian.it@gmail.com>
2025-11-30 17:50:33 +0100

    refactor: delete unnecessary second migration folder

--- backend/migration/V11__add_genres_to_album.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE album_genres
-(
-    album_id BIGINT       NOT NULL,
-    genre    VARCHAR(255) DEFAULT 'POP' NOT NULL
-);
-
-ALTER TABLE album_genres
-    ADD CONSTRAINT fk_album_genres_on_album FOREIGN KEY (album_id) REFERENCES albums (id);
