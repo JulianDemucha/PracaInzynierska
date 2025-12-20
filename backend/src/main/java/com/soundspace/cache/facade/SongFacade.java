d7cfd51e731922ed311d0a66851e0fa96826aeca
JulianDemucha <demuchajulian.it@gmail.com>
2025-12-20 10:21:07 +0100

    refactor(song): rename SongEnrichmentService to SongAssembler

--- backend/src/main/java/com/soundspace/cache/facade/SongFacade.java
+++ backend/src/main/java/com/soundspace/cache/facade/SongFacade.java
@@ -1,7 +1,7 @@
 package com.soundspace.cache.facade;
 import com.soundspace.cache.provider.CachedSongsProvider;
 import com.soundspace.dto.SongDtoWithDetails;
-import com.soundspace.service.song.SongEnrichmentService;
+import com.soundspace.util.SongAssembler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class SongFacade {
 
     private final CachedSongsProvider cachedSongsProvider;
-    private final SongEnrichmentService songEnrichmentService;
+    private final SongAssembler songEnrichmentService;
 
     public SongDtoWithDetails getSong(Long songId, UserDetails userDetails) {
         return songEnrichmentService.addStatistics(cachedSongsProvider.getSong(songId, userDetails));
