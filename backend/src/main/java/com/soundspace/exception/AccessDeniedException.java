c45bd26f6fad6b1721a6c793a6b74c3ac2557893
JulianDemucha <demuchajulian.it@gmail.com>
2025-12-21 18:38:05 +0100

    fix(exception): handle AccesDeintedException to 403 instead of 401

--- backend/src/main/java/com/soundspace/exception/AccessDeniedException.java
+++ backend/src/main/java/com/soundspace/exception/AccessDeniedException.java
@@ -3,7 +3,7 @@ package com.soundspace.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class AccessDeniedException extends RuntimeException {
     public AccessDeniedException(String message) {
         super(message);
